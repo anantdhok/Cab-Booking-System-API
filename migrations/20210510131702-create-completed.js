595e0f77bd6838d2dc7ff896e0d4fd6fd424cb22
Anant Dhok <67429427+anantdhok@users.noreply.github.com>
2021-05-11 02:03:37 +0530

    Add files via upload

--- migrations/20210510131702-create-completed.js
+++ migrations/20210510131702-create-completed.js
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("Complete ds");
+    await queryInterface.dropTable("Completeds");
   },
 };
