7138430a9ed996c8543ce32b486321d171e47d89
Anant Dhok <67429427+anantdhok@users.noreply.github.com>
2021-05-11 02:01:32 +0530

    Update ride.js

--- routes/ride.js
+++ routes/ride.js
@@ -2,85 +2,63 @@ const express = require("express");
 const db = require("../models/index");
 
 // Database Reference
-const { Ongoing, Completed } = db;
+const { User, Ongoing, Completed } = db;
 const router = express.Router();
 
-// Register API
+// Book New Ride
 router.post("/book", (req, res) => {
   if (req.body.driver && req.body.passenger)
-    Ongoing.findOne({ where: { driver: req.body.driver } || { passenger: req.body.passenger }, raw: false })
+    Ongoing.findAll({ where: { driver: req.body.driver } || { passenger: req.body.passenger }, raw: false })
       .then((ride) => {
         if (ride) res.status(401).json({ success: false, message: "Ride cannot be booked" });
-        else {
+        else
           Ongoing.create({
             driver: req.body.driver,
             passenger: req.body.passenger,
             origin: { type: "Point", coordinates: [39.807222, -76.984722] },
             destination: { type: "Point", coordinates: [39.807222, -76.984722] },
           })
-            .then((rideNew) => {
-              res.status(200).json({
-                success: true,
-                result: rideNew,
-              });
-            })
-            .catch(() => {
-              res.status(401).json({ success: false, message: "Error Booking Ride" });
-            });
-        }
+            .then((rideNew) => res.status(200).json({ success: true, result: rideNew }))
+            .catch(() => res.status(401).json({ success: false, message: "Error Booking Ride" }));
       })
-      .catch((err) => {
-        res.status(401).json({ success: false, message: err });
-      });
+      .catch((err) => res.status(401).json({ success: false, message: err }));
   else res.status(401).json({ success: false, message: "Insufficient Information to Book" });
 });
 
 // Check Available Cabs
-router.post("/check", (req, res, done) => {
-  return Ongoing.find({ where: { type: "driver" }, raw: false }).then((result) => {
-    res.status(200).json({
-      success: true,
-      message: result,
-    });
-  });
+router.get("/check", (req, res) => {
+  User.findAll({ where: { type: "driver" }, raw: false })
+    .then((result) => res.status(200).json({ success: true, message: result }))
+    .catch(() => res.status(401).json({ success: false, message: "Cabs not available" }));
 });
 
-// Get ride API
+// Finish Ongoing Rides
 router.post("/finish/:id", (req, res) => {
-  const clientId = req.params.id;
-  Ongoing.findOne({ where: { id: clientId }, raw: false }).then((result) => {
-    Ongoing.deleteOne({ where: { id: result.id }, raw: false })
-      .then((result) => {
+  const travelId = req.params.id;
+  Ongoing.findOne({ where: { id: travelId }, raw: false })
+    .then((result) => {
+      if (result !== {})
         Completed.create({
           driver: result.driver,
           passenger: result.passenger,
-          origin: result.passenger,
-          destination: result.passenger,
+          origin: result.origin,
+          destination: result.destination,
         })
-          .then((rideNew) => {
-            res.status(200).json({
-              success: true,
-              result: rideNew,
-            });
+          .then((result) => {
+            Ongoing.delete({ where: { id: travelId }, raw: false })
+              .then(() => res.status(200).json({ success: true, result: "Ride is ended" }))
+              .catch(() => res.status(401).json({ success: false, message: "Error Finishing Ride 3" }));
           })
-          .catch(() => {
-            res.status(401).json({ success: false, message: "Error Finishing Ride" });
-          });
-      })
-      .catch(() => {
-        res.status(401).json({ success: false, message: "Error Finishing Ride" });
-      });
-  });
+          .catch(() => res.status(401).json({ success: false, message: "Error Finishing Ride 2" }));
+    })
+    .catch(() => res.status(401).json({ success: false, message: "Error Finishing Ride 1" }));
 });
 
-// Get ride API
+// Get Completed Rides
 router.get("/completed", (req, res) => {
-  return Ongoing.findOne({ where: { id: clientId }, raw: false }).then((result) => {
-    res.status(200).json({
-      success: true,
-      message: result,
-    });
-  });
+  Completed.findAll({ where: { passenger: req.body.id }, raw: false })
+    .then((result) => res.status(200).json({ success: true, message: result }))
+    .catch((err) => res.status(401).json({ success: false, message: err }));
 });
 
 module.exports = router;
