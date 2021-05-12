286aa5fe46e6da4f3092af26c311ef04677bd27b
Anant Dhok <67429427+anantdhok@users.noreply.github.com>
2021-05-12 22:35:47 +0530

    Added files via upload

--- routes/ride.js
+++ routes/ride.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const db = require("../models/index");
+const { Op } = require("sequelize");
 
 // Database Reference
 const { User, Ongoing, Completed } = db;
@@ -7,28 +8,41 @@ const router = express.Router();
 
 // Book New Ride
 router.post("/book", (req, res) => {
-  if (req.body.driver && req.body.passenger)
-    Ongoing.findAll({ where: { driver: req.body.driver } || { passenger: req.body.passenger }, raw: false })
+  if (req.body.driver && req.body.passenger && req.body.origin && req.body.destination)
+    Ongoing.findAll({
+      where: {
+        [Op.and]: [
+          { driver: { [Op.or]: [req.body.driver, req.body.passenger] } },
+          { passenger: { [Op.or]: [req.body.driver, req.body.passenger] } },
+        ],
+      },
+      raw: false,
+    })
       .then((ride) => {
-        if (ride) res.status(401).json({ success: false, message: "Ride cannot be booked" });
+        if (ride === {}) res.status(401).json({ success: false, message: "Ride cannot be booked" });
         else
           Ongoing.create({
             driver: req.body.driver,
             passenger: req.body.passenger,
-            origin: { type: "Point", coordinates: [39.807222, -76.984722] },
-            destination: { type: "Point", coordinates: [39.807222, -76.984722] },
+            origin: { type: "Point", coordinates: [req.body.origin.x, req.body.origin.y] },
+            destination: { type: "Point", coordinates: [req.body.destination.x, req.body.destination.y] },
           })
             .then((rideNew) => res.status(200).json({ success: true, result: rideNew }))
-            .catch(() => res.status(401).json({ success: false, message: "Error Booking Ride" }));
+            .catch(() => res.status(401).json({ success: false, message: "Cannot process request" }));
       })
-      .catch((err) => res.status(401).json({ success: false, message: err }));
-  else res.status(401).json({ success: false, message: "Insufficient Information to Book" });
+      .catch((err) => res.status(401).json({ success: false, message: "Cannot process request" }));
+  else res.status(401).json({ success: false, message: "Insufficient information to book" });
 });
 
 // Check Available Cabs
 router.get("/check", (req, res) => {
-  User.findAll({ where: { type: "driver" }, raw: false })
-    .then((result) => res.status(200).json({ success: true, message: result }))
+  User.findAll({
+    where: sequelize.literal("(posts.id NOT IN (SELECT B.driver FROM Ongoings B WHERE B.userId=" + userId + "))"),
+    raw: false,
+  })
+    .then((result) => {
+      res.status(200).json({ success: true, message: result });
+    })
     .catch(() => res.status(401).json({ success: false, message: "Cabs not available" }));
 });
 
@@ -44,14 +58,18 @@ router.post("/finish/:id", (req, res) => {
           origin: result.origin,
           destination: result.destination,
         })
-          .then((result) => {
-            Ongoing.delete({ where: { id: travelId }, raw: false })
+          .then(() => {
+            User.update(
+              { location: result.destination },
+              { where: { id: { [Op.or]: [result.driver, result.passenger] } }, raw: false }
+            );
+            Ongoing.destroy({ where: { id: travelId }, raw: false })
               .then(() => res.status(200).json({ success: true, result: "Ride is ended" }))
-              .catch(() => res.status(401).json({ success: false, message: "Error Finishing Ride 3" }));
+              .catch(() => res.status(401).json({ success: false, message: "Cannot process request" }));
           })
-          .catch(() => res.status(401).json({ success: false, message: "Error Finishing Ride 2" }));
+          .catch(() => res.status(401).json({ success: false, message: "Cannot process request" }));
     })
-    .catch(() => res.status(401).json({ success: false, message: "Error Finishing Ride 1" }));
+    .catch(() => res.status(401).json({ success: false, message: "Cannot process request" }));
 });
 
 // Get Completed Rides
@@ -62,4 +80,3 @@ router.get("/completed", (req, res) => {
 });
 
 module.exports = router;
- 
